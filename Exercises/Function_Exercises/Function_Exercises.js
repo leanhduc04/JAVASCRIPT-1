da6e793f5a2629d64f171eca183b4ab8890b6272
Ledaian2342 <ledaian22@gmail.com>
2022-07-07 20:56:46 +0700

    add time

--- Exercises/Function_Exercises/Function_Exercises.js
+++ Exercises/Function_Exercises/Function_Exercises.js
@@ -1,5 +1,7 @@
 (function () {
     reverseString();
+    minMax();
+    time();
 })()
 
 
@@ -32,5 +34,50 @@ function reverseString() {
     var arrs2 = arrs.join('');
     console.log(arrs2);
 }
+
+// Tạo một function nhận vào một mảng. sau đó trả về một phần tử ngẫu nhiên trong mảng đó
+// Đầu vào: [32, “abc", true, 65]
+// Trả về: true
+// --------------------
+// Đầu vào: [32, “abc", true, 65]
+// Trả về: 65
+
+function minMax() {
+    var arrs = [32, "abc", true, 65];
+    var arr = arrs[Math.floor(Math.random() * arrs.length)];
+    console.log(arr);
+}
+
+// Tạo một function nhận vào một số là số giờ (tính theo 24h). Trả về một chuỗi xác định với số giờ đó thì là sáng, trưa hay tối
+// Đầu vào: 12
+// Trả về: “Trưa"
+// --------------------
+// Đầu vào: 6
+// Trả về: “Sáng"
+
+function time() {
+    var hours = 6;
+    var hour = hours > 0 && hours <=24;
+    
+    if (hours < 10)
+    {
+        hours = "Sáng";
+    }
+    else if (hours <= 10 && hour <=12)
+    {
+        hours = "Trưa";
+    }
+    else if (hours <= 12 && hour <18)
+    {
+        hours = "Chiều";
+    }
+    else
+    {
+        hours = "Tối";
+    }
+
+    console.log(hours);
+}
+
 
 
