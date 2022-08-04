34d47247cf17efa9480f576f23417410984032a2
Ledaian2342 <ledaian22@gmail.com>
2022-08-04 20:33:38 +0700

    90 percent Scope_EX

--- Exercises/Scope_Exercises/script.js
+++ Exercises/Scope_Exercises/script.js
@@ -2,15 +2,27 @@
 function test() {
     console.log(a);
     console.log(foo());
-
     var a = 1;
     function foo() {
         return 2;
     }
 }
-
 test();
+// => ra undefined vì log trước khi khai báo biến a
+// => ra 2 vì func return về 2 
 
-// => ra undefined vì a ở scope lớn //hơn
-// =>  
+//-----------------------+++++++++-----------------------//
 
+// 2. What is result?
+var a = 1;
+function someFunction(number) {
+    function otherFunction(input) {
+        return a;
+    }
+    a = 5;
+    return otherFunction;
+}
+var firstResult = someFunction(9);
+var result = firstResult(2);
+
+// => ra 5 vì func trả về a và sau đó gán lại giá trị của a = 5
